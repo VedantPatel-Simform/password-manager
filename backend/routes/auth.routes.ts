3559bb69a761474947ecad17216d73296f51a625
vedantpatel-simform <vedant.patel@simformsolutions.com>
2025-07-15 11:21:50 +0530

    feat : implemented rate limiter for auth routes

--- backend/routes/auth.routes.ts
+++ backend/routes/auth.routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import {
     registerController,
     loginController,
@@ -7,10 +8,32 @@ import {
 } from '../controllers/authentication.controller.js';
 import { validate } from '../middlewares/zodValidator.middleware.js';
 import { registerSchema, loginSchema } from '../validators/auth.validators.js';
+
 const router = express.Router();
 
-router.post('/register', validate(registerSchema), registerController);
-router.post('/login', validate(loginSchema), loginController);
+const authLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 5, // Limit each IP to 5 requests per window
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: (req, res) => {
+        res.status(429).json({
+            success: false,
+            message: 'Too many requests, please try again later.',
+            statusCode: 429,
+        });
+    },
+});
+
+router.post(
+    '/register',
+    authLimiter,
+    validate(registerSchema),
+    registerController
+);
+router.post('/login', authLimiter, validate(loginSchema), loginController);
+
 router.get('/check-session', checkSessionController);
 router.post('/logout', logoutController);
+
 export default router;
